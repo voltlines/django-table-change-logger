7180f05a9f8799da7fe7dc77c2eafb2b36852093
canarsoy <can@voltlines.com>
2019-08-05 18:29:57 +0300

    Change setup file

--- setup.py
+++ setup.py
@@ -16,9 +16,8 @@ setuptools.setup(
     license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2.7"
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
     install_requires=[
         'Django>=1.11',
